6d7b1c3e59f4dc0f3e793e77696bed7d9483bd89
Traximus <traximus.td@gmail.com>
2014-12-04 12:33:17 +0800

    Initial Commit

---
+++ HTTPTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPTest
+//
+//  Created by traximus on 14-12-4.
+//  Copyright (c) 2014年 netcoretec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPTest
+//
+//  Created by traximus on 14-12-4.
+//  Copyright (c) 2014年 netcoretec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
